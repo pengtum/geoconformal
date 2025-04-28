6f5de2c483af88c2025835c1548024638a2ade97
pengluo-gis <pluopku@gmail.com>
2025-04-28 18:04:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='geoconformal',
-    version='0.1.0',
+    version='0.1.1',
     description='Geographically Weighted Conformal Prediction Methods',
     author='Peng Luo',
     author_email='pengluo@mit.edu',
